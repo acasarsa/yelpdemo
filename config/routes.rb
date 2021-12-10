2e92d17d003b78da31d824df3c2ebef20e725edc
Andrew Casarsa <acasarsa@gmail.com>
2021-12-10 17:35:17 -0500

    restaurants resources created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :restaurants
   get 'pages/about'
   get 'pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20211210220117_create_restaurants.rb
@@ -0,0 +1,16 @@
+class CreateRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :phone
+      t.string :website
+
+      t.timestamps
+      t.index :name
+      t.index :address
+      t.index :phone
+      t.index :website
+    end
+  end
+end
