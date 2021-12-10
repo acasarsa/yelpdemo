4f1d7a804e9474eb71185026ed07934554578b5d
Andrew Casarsa <acasarsa@gmail.com>
2021-12-10 16:54:26 -0500

    config MiniProfiler gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   Rack::MiniProfiler.config.position = 'bottom-right'
-  Rack::MiniProfiler.config.start_hidden = true
+  # Rack::MiniProfiler.config.start_hidden = true
 end
